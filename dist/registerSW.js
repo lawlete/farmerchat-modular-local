94c8c71a1dbc4a83c90a77ba792ec01323b63ffb
Lawlete <alfredolawler@gmail.com>
2025-05-26 02:42:16 -0300

    Corrigiendo configuración de base en vite.config.ts

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/farmerchat-modular/sw.js', { scope: '/farmerchat-modular/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/farmerchat-modular-local/sw.js', { scope: '/farmerchat-modular-local/' })})}
